2be68039cec9166a0d96526a586a98448e6059b7
trang.le <tranglt@TrangLTT.local>
2025-04-22 21:45:33 +0700

    fix: working api for send message

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,10 +1,12 @@
 import { protectRoute } from "../middleware/auth.middleware.js";
-import express from express;
-import { getMessages, getUsersForSideBar } from "../controllers/message.controller.js";
+import express from "express";
+import { getMessages, getUsersForSideBar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSideBar)
 router.get("/:id", protectRoute, getMessages)
+
+router.post("/send/:id", protectRoute, sendMessage)
 
 export default router;
