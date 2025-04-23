53a69105eace425126a31bae623533df417f88a6
trang.le <tranglt@TrangLTT.local>
2025-04-23 22:27:24 +0700

    fix: add cors in BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,30 @@
-
-import express from  'express';
+import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import dotenv from 'dotenv';
-import { connectDB } from './lib/db.js';
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes)
+app.use("/api/message", messageRoutes);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on PORT: ${PORT}`);
+  connectDB();
+});
