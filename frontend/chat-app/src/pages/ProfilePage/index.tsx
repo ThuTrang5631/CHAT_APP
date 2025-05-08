ba0fbd7b503257b1bbfbb11e70f717efcb3aa7b6
trang.le <thutrang5631@gmail.com>
2025-05-08 22:06:56 +0700

    fix: add UI for profile page

--- frontend/chat-app/src/pages/ProfilePage/index.tsx
+++ frontend/chat-app/src/pages/ProfilePage/index.tsx
@@ -1,5 +1,59 @@
-const ProfilePage = () =>{
-    return <></>
-}
+import { Camera } from "lucide-react";
+import loginImage from "../../assets/login.jpg";
+import { useAuthStore } from "../../store/useAuthStore";
+
+const ProfilePage = () => {
+  const { authUser } = useAuthStore();
+
+  const handleChangeInputFile = (e: any) => {
+    console.log("value", e);
+  };
+
+  const handleUploadProfile = async () => {
+    try {
+    } catch (error) {}
+  };
+
+  return (
+    <div className="profile-page">
+      <div className="profile-page-container">
+        <div className="profile-section">
+          <h1 className="profile-page-title">Profile</h1>
+          <p className="profile-page-desc">Your profile information</p>
+        </div>
+        <div className="profile-section">
+          <div className="profile-section-avatar">
+            <img
+              src={loginImage}
+              alt="profile-image"
+              className="profile-page-image"
+            />
+
+            <span className="profile-page-icon">
+              <input
+                type="file"
+                id="file"
+                className="profile-page-input"
+                onChange={(e) => handleChangeInputFile(e)}
+              />
+            </span>
+          </div>
+          <div className="flex flex-col input-item w-full">
+            <label className="label-input">Full Name</label>
+            <input
+              value={authUser?.fullName}
+              className="input"
+              readOnly={true}
+            />
+          </div>
+          <div className="flex flex-col input-item w-full">
+            <label className="label-input">Email</label>
+            <input className="input" readOnly={true} value={authUser?.email} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProfilePage;
--- frontend/chat-app/src/store/useAuthStore.ts
+++ frontend/chat-app/src/store/useAuthStore.ts
@@ -1,19 +1,32 @@
 import { create } from "zustand";
 import { request } from "../lib/axios";
 
-export const useAuthStore = create((set) => ({
+interface AuthUser {
+  id: string;
+  email: string;
+  fullName: string;
+}
+
+interface AuthStore {
+  authUser: AuthUser | null;
+  isSigningUp: boolean;
+  isLoggingIn: boolean;
+  isUpdateProfile: boolean;
+  isCheckingAuth: boolean;
+  saveAuthUser: (newUser: AuthUser) => void;
+  checkAuth: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
   isUpdateProfile: false,
-
   isCheckingAuth: true,
-  saveAuthUser: (newUser: any) => set({ authUser: newUser }),
+  saveAuthUser: (newUser: AuthUser | null) => set({ authUser: newUser }),
   checkAuth: async () => {
     try {
       const res = await request.get("/auth/check");
-      console.log(res);
-
       set({ authUser: res.data });
     } catch (error) {
       console.log("Error in checkAuth", error);
